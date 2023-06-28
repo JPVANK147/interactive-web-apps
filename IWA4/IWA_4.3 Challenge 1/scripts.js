7907d38e645b39c367c0f3c3bd463ac8169c85d4
JP <jpvank@gmail.com>
2023-06-28 17:37:33 +0200

    Work

--- IWA4/IWA_4.3 Challenge 1/scripts.js
+++ IWA4/IWA_4.3 Challenge 1/scripts.js
@@ -1,9 +1,9 @@
 let date = "month"
-let status = "student"
+let status = "parent"
 let count = 0
 let year = 2050
 
-if (date = "month") {
+if (date == "month") {
 	console.log("January", 'New Year’s Day')
 	console.log("March", 'Women’s Day')
 	date = 'April'
@@ -11,7 +11,7 @@ if (date = "month") {
 	console.log(date, 'Freedom Day')
 	count = count + 4
 
-	if (status = "student") {
+	if (status == "student") {
 	  	console.log('August', 'Youth Day')
 		count = count + 1
   	}
@@ -22,7 +22,7 @@ if (date = "month") {
 	console.log(date, 'Day of Reconciliation')
 	count = count + 3
 
-	if (status = "parent") {
+	if (status == "parent") {
 	  	console.log(date, 'Christmas Day')
 		count = count + 1
   	}
