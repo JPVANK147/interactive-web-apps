a967eb5474e9630537be049b4d55769d4e3cf178
JP <jpvank@gmail.com>
2023-07-01 13:26:14 +0200

    Work

--- IWA5/IWA_5.2 Challenge 1/script.js
+++ IWA5/IWA_5.2 Challenge 1/script.js
@@ -1,45 +1,43 @@
-const FREE_WARNING = 'Free shipping only applies to single customer orders';
-const BANNED_WARNING = 'Unfortunately, we do not ship to your country of residence';
-const NONE_SELECTED = '0';
+const FREE_WARNING = 'Free shipping only applies to single customer orders'
+const BANNED_WARNING = 'Unfortunately, we do not ship to your country of residence'
+const NONE_SELECTED = '0'
 
-let shipping = null;
-let currency = '$';
+let shipping = null
+let currency = '$'
 
-const location = 'RSA';
-const customers = 1;
-const calcShipping = 1500
+const userLocation = 'RSA'
+const customers = 1
 
-let shoes = 300 * 1;
-let toys = 100 * 5;
-let shirts = 150 * NONE_SELECTED;
-let batteries = 35 * 2;
-let pens = 5 * NONE_SELECTED;
+let shoes = 300 * 1
+let toys = 100 * 5
+let shirts = 150 * parseInt(NONE_SELECTED)
+let batteries = 35 * 2
+let pens = 5 * parseInt(NONE_SELECTED)
 
-if (location === 'RSA') {
-  shipping = 400;
-  currency = 'R';
+if (userLocation === 'RSA') {
+  shipping = 400
+  currency = 'R'
 }
 
-if (location === 'NAM') {
-  shipping = 600;
+if (userLocation === 'NAM') {
+  shipping = 600
 } else {
-  shipping = 800;
+  shipping = 800
 }
 
-if (shoes + toys + batteries + pens + shirts > 1000) {
-  if (location === 'NAM' && customers < 2) {
-    if (location === 'RSA') {
-      shipping = 0 || calcShipping
-    }
-  }
-}
+if (shoes + toys + batteries + pens + shirts > 1000 ) {
+	if (userLocation = NAM && customers < 2) {
+			if (userLocation = RSA)
+		    shipping = 0 || calcShipping
+		}
+	}
 
 if (shipping === 0 && customers !== 1) {
-  console.log(FREE_WARNING);
+  console.log(FREE_WARNING)
 }
 
-if (location === 'NK') {
-  console.log(BANNED_WARNING);
+if (userLocation === 'NK') {
+  console.log(BANNED_WARNING)
 } else {
-  console.log('price', currency, shoes + batteries + pens + shirts + shipping);
+  console.log('Price:', currency + (shoes + batteries + pens + shirts + shipping))
 }
