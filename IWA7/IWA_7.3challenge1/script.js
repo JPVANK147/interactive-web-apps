a5b9b208f841f28854398b65e97c675e3c461a8d
JP <jpvank@gmail.com>
2023-07-08 16:45:28 +0200

    Work

--- IWA7/IWA_7.3challenge1/script.js
+++ IWA7/IWA_7.3challenge1/script.js
@@ -1,2 +1,5 @@
-const value = "3"
+const value = 3
 console.log(value + 4 + value)
+
+// So basis the "" indicate it a string value, you cant plus a string and a number together, that why is console log 343
+// If it a number value like 3 it will plus all together with the + operator   
