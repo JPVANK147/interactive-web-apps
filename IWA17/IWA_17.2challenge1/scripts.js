e1aab70211e3dbe8c001beb9b4eb98ee43afb649
JP <jpvank@gmail.com>
2023-08-10 12:46:20 +0200

    Work

--- IWA17/IWA_17.2challenge1/scripts.js
+++ IWA17/IWA_17.2challenge1/scripts.js
@@ -15,29 +15,54 @@ const MONTHS = [
     'December',
 ]
 
-const getDaysInMonth = (date) => new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
+const getDaysInMonth = (date) => new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate() // 2023 - 08 -0
 
 // Only edit below 
 
+/**
+ * So this let you add the days of the week 
+ */
 const createArray = (length) => {
     const result = []
 
-    for (let  = ; i < length; i) {
-        result.push()
+    for (let i = 0; i < length; i = i + 1) {
+        result.push(i)
     }
 
     return result
 }
 
-const createData = () > {
-    const current = new ()
-    current.set(1)
 
-    const startDay = current.get()
-    const daysInMonth = getDaysInMonth(current)
+
+const createData = () => {
+    const currentDate = new Date() // Represents the current date and time
+    currentDate.setDate(1) // Tue Aug 01 2023 12:11:16 GMT+0200 (Central Africa Time). So the getDate(1) this will make that the month begin with 01.
+
+    const startDay = currentDate.getDay() // Get the day of the week for a specific date.  2 
+    const daysInMonth = getDaysInMonth(currentDate) // calculates and assigns the number of days in the month of the currentDate. Exp 31
 
     const weeks = createArray(5)
-    const days = ()
+    /** this wili make a array 0-5 for the weeks of the calendar in the createArray for statement
+     * Array(5) [ 0, 1, 2, 3, 4 ]
+        0: 0
+        1: 1
+        2: 2
+        3: 3
+        4: 4
+        length: 5
+     */
+    const days = createArray(7)
+    /** this wil make a array 0-7 for the days of the calendar in the createArray for statement
+     * Array(7) [ 0, 1, 2, 3, 4, 5, 6 ]
+        0: 0
+        1: 1
+        2: 2
+        3: 3
+        4: 4
+        5: 5
+        6: 6
+        length: 7
+     */
     const result = []
 
     for (const weekIndex of weeks) {
@@ -46,8 +71,8 @@ const createData = () > {
             days: []
         })
 
-        for (const  of ) {
-            const day = (dayIndex - startDay) : (weekIndex * 7)  1
+        for (const dayIndex of days) {
+            const day = (dayIndex - startDay) + (weekIndex * 7) + 1
             const isValid = day > 0 && day <= daysInMonth
 
             result[weekIndex].days.push({
@@ -75,16 +100,16 @@ const addCell = (existing, classString, value) => {
 const createHtml = (data) => {
     let result = ''
 
-    for (const { week,  } of ) {
+    for (const { week, } of ) {
         let inner = ""
         inner = addCell(inner, 'table__cell table__cell_sidebar', `Week ${week}`)
-    
-         (const { dayOfWeek, value } of days) {
+
+            (const { dayOfWeek, value } of days) {
             const isToday = new Date().getDate() === value
             const isWeekend = dayOfWeek ===  | dayOfWeek === 
             const isAlternate = week % 2 === 0
-            
-						let classString = 'table__cell'
+
+            let classString = 'table__cell'
 
             if () classString = `${} table__cell_`
             if () classString = `${} table__cell_`
@@ -97,7 +122,7 @@ const createHtml = (data) => {
             <tr>${inner}</tr>
         `
     }
-    
+
     return result
 }
 
