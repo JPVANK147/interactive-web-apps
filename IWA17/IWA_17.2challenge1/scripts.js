52dc8e6d0076a85f20dd7203cfa1a9d16fdb4c30
JP <jpvank@gmail.com>
2023-08-11 11:31:13 +0200

    Work

--- IWA17/IWA_17.2challenge1/scripts.js
+++ IWA17/IWA_17.2challenge1/scripts.js
@@ -32,8 +32,6 @@ const createArray = (length) => {
     return result
 }
 
-
-
 const createData = () => {
     const currentDate = new Date() // Represents the current date and time
     currentDate.setDate(1) // Tue Aug 01 2023 12:11:16 GMT+0200 (Central Africa Time). So the getDate(1) this will make that the month begin with 01.
@@ -64,12 +62,27 @@ const createData = () => {
         length: 7
      */
     const result = []
-
+    /** 
+     *  this is a for.. of statement that represent the weeks in the month. 
+     *  week - represents the week number. 
+     *  weekIndex + 1 used here to make a week number, it start by 1.
+     *  days - it  a empty array that will be filled with days for each week.
+     */
     for (const weekIndex of weeks) {
         result.push({
             week: weekIndex + 1,
             days: []
         })
+        /**
+         * this is a for.. of statement that represent the days in the weeks. 
+         * (dayIndex - startDay) - adjusts for the day of the week that the month starts on. like  Tuesday (dayIndex 2) subtraction first day of the month would be 1.  
+         * (weekIndex * 7) - move the dayIndex to the correct day within the current week.
+         * isValid - check whether the calculated day value is within the range of valid days for the current month.
+         * 
+         * dayOfWeek -  Represents the day of the week index (0 to 6)
+         * value - Represents the day of the month. If the calculated day is valid 
+         * isValid is true - day number is used. Otherwise, an empty string is used to represent an invalid day.
+         */
 
         for (const dayIndex of days) {
             const day = (dayIndex - startDay) + (weekIndex * 7) + 1
@@ -87,8 +100,7 @@ const createData = () => {
 
 const addCell = (existing, classString, value) => {
     const result = /* html */ `
-        ${}
-
+        ${existing}
         <td class="${classString}">
             &nbsp;${value}&nbsp;
         </td>
@@ -100,21 +112,21 @@ const addCell = (existing, classString, value) => {
 const createHtml = (data) => {
     let result = ''
 
-    for (const { week, } of ) {
+    for (const { week, days } of data) {
         let inner = ""
         inner = addCell(inner, 'table__cell table__cell_sidebar', `Week ${week}`)
 
-            (const { dayOfWeek, value } of days) {
+        for (const { dayOfWeek, value } of days) {
             const isToday = new Date().getDate() === value
-            const isWeekend = dayOfWeek ===  | dayOfWeek === 
+            const isWeekend = dayOfWeek === 0 || dayOfWeek === 6
             const isAlternate = week % 2 === 0
 
             let classString = 'table__cell'
 
-            if () classString = `${} table__cell_`
-            if () classString = `${} table__cell_`
-            if () classString = `${} table__cell_`
-            inner = addCell()
+            if (isToday) classString = `${classString} table__cell_`
+            if (isWeekend) classString = `${classString} table__cell_`
+            if (isAlternate) classString = `${classString} table__cell_`
+            inner = addCell(inner, classString, value)
         }
 
         result = `
