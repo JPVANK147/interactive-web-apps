f03e026c819b59789456ace56034eef81c2573ea
JP <jpvank@gmail.com>
2023-08-21 10:41:42 +0200

    Work

--- IWA18/IWA_18.4challenge1/IWA_18_4/scripts.js
+++ IWA18/IWA_18.4challenge1/IWA_18_4/scripts.js
@@ -9,6 +9,20 @@
  *
  * @param {Event} event 
  */
+import {
+    state,
+    createOrderData,
+    COLUMNS,
+    TABLES
+} from './data.js'
+
+import {
+    html,
+    createOrderHtml,
+    updateDraggingHtml,
+    moveToColumn
+} from './view.js'
+
 const handleDragOver = (event) => {
     event.preventDefault();
     const path = event.path || event.composedPath()
@@ -26,16 +40,113 @@ const handleDragOver = (event) => {
     updateDragging({ over: column })
     updateDraggingHtml({ over: column })
 }
+// Drag start 
+const handleDragStart = (event) => {
+    const orderId = event.target.dataset.id
+    event.dataTransfer.setData('text/plain', orderId)
+    event.target.classList.add('dragging')
+}
+// Drag drop 
+const handleDragEnd = (event) => {
+    event.target.classList.remove('dragging')
+}
+// Add the "Help" button overlay
+const handleHelpToggle = (event) => {
+    if (html.help.overlay.hasAttribute('open')) {
+        html.help.overlay.removeAttribute('open')
+    } else {
+        html.help.overlay.setAttribute('open', '')
+    }
+}
+// Add the "Add order" overlay
+const handleAddToggle = (event) => {
+    if (html.add.overlay.hasAttribute('open')) {
+        html.add.overlay.removeAttribute('open')
+    } else {
+        html.add.overlay.setAttribute('open', '')
+    }
+}
+// Add the order number submit button
+const handleAddSubmit = (event) => {
+    event.preventDefault()
+    const title = html.add.title.value
+    const table = html.add.table.value
 
+    if (title && table) {
+        const order = createOrderData({ title, table, column: 'ordered' })
+        state.orders[order.id] = order
+        html.columns.ordered.appendChild(createOrderHtml(order))
+        html.add.overlay.removeAttribute('open')
+    }
+}
+// Edit the edit toggle overlay
+const handleEditToggle = (event) => {
+    const orderId = event.target.closest('.order')?.getAttribute('data-id');
+    if (orderId) {
+        const order = state.orders[orderId]
+        html.edit.id.value = order.id
+        html.edit.title.value = order.title
+        html.edit.table.value = order.table
+        html.edit.column.value = order.column
+        html.edit.overlay.setAttribute('open', '')
+        html.edit.title.focus();
+    } else {
+        if (html.edit.overlay.hasAttribute('open')) {
+            html.edit.overlay.removeAttribute('open')
+            html.other.add.focus()
+        }
+    }
+};
+// Edit the edit submit overlay
+const handleEditSubmit = (event) => {
+    event.preventDefault()
+    const id = html.edit.id.value
+    const title = html.edit.title.value
+    const table = html.edit.table.value
+    const column = html.edit.column.value
 
-const handleDragStart = (event) => {}
-const handleDragEnd = (event) => {}
-const handleHelpToggle = (event) => {}
-const handleAddToggle = (event) => {}
-const handleAddSubmit = (event) => {}
-const handleEditToggle = (event) => {}
-const handleEditSubmit = (event) => {}
-const handleDelete = (event) => {}
+    if (title && table && column) {
+        const order = state.orders[id]
+        if (!order) return
+
+        order.title = title
+        order.table = table
+        order.column = column
+
+        document.querySelector(`[data-id="${id}"]`).querySelector('[data-order-title]').textContent = title
+        document.querySelector(`[data-id="${id}"]`).querySelector('[data-order-table]').textContent = table
+
+        moveToColumn(id, column)
+        updateDraggingHtml({ over: column })
+
+        html.edit.overlay.removeAttribute('open')
+    }
+}
+// Edit the Delete in the Edit Order overlay
+const handleDelete = (event) => {
+    const orderDelete = document.querySelector(`[data-id="${html.edit.id.value}"]`)
+    if (orderDelete) {
+        orderDelete.remove()
+        delete state.orders[html.edit.id.value]
+    }
+
+    html.edit.overlay.removeAttribute('open')
+}
+// This is the drag and drop to a targer column 
+const handleDrop = (event) => {
+    event.preventDefault()
+    const orderId = event.dataTransfer.getData('text/plain')
+    const targetColumn = event.target.closest('[data-area]')?.dataset.area
+
+    if (orderId && targetColumn) {
+       
+        state.orders[orderId].column = targetColumn
+
+        moveToColumn(orderId, targetColumn)
+        
+        updateDraggingHtml({ over: null })
+    }
+}
 
 html.add.cancel.addEventListener('click', handleAddToggle)
 html.other.add.addEventListener('click', handleAddToggle)
@@ -56,4 +167,5 @@ for (const htmlColumn of Object.values(html.columns)) {
 
 for (const htmlArea of Object.values(html.area)) {
     htmlArea.addEventListener('dragover', handleDragOver)
+    htmlArea.addEventListener('drop', handleDrop)
 }
